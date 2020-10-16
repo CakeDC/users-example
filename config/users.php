574049c70e8acb4103419865150cdfa2537aae82
Jorge González <steinkel@gmail.com>
2020-10-16 09:05:21 +0100

    customize password hasher to use FallbackPasswordHasher

--- config/users.php
+++ config/users.php
@@ -1,5 +1,29 @@
 <?php
 $config = [
+    'Auth.Identifiers' => [
+        'Password' => [
+            'className' => 'Authentication.Password',
+            'fields' => [
+                'username' => ['username', 'email'],
+                'password' => 'password'
+            ],
+            'resolver' => [
+                'className' => 'Authentication.Orm',
+                'finder' => 'active'
+            ],
+            'passwordHasher' => [
+                'className' => 'Authentication.Fallback',
+                'hashers' => [
+                    'Authentication.Default',
+                    [
+                        'className' => 'Authentication.Legacy',
+                        'hashType' => 'md5',
+                        'salt' => false // turn off default usage of salt
+                    ],
+                ]
+            ]
+        ],
+    ]
 ];
 
 return $config;
